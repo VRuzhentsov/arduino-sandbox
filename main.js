e7bfa999d23fb72e3f824a0dd32b2f6d816d8c65
vruzhentsov <v.ruzhentsov@gmail.com>
2019-04-12 19:05:58 +0300

    Online color changer

--- main.js
+++ main.js
@@ -2,6 +2,67 @@ const {Board: FirmataBoard} = require('firmata');
 const {Board: J5Board, Led: {RGB: RGBLed}} = require("johnny-five");
 const {SerialPort: VirtualSerialPort} = require('udp-serial');
 
+const app = require('http').createServer(handler)
+const ioServer = require('socket.io')(app);
+const fs = require('fs');
+
+const path = require('path');
+
+app.listen(8080);
+
+function handler (request, response) {
+    console.log('request starting...');
+    const clientFolder = '/client-app';
+
+    var filePath =`.${clientFolder}${request.url}`;
+    if (filePath == `.${clientFolder}/`)
+        filePath = `.${clientFolder}/index.html`;
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}
+
 //create the udp serialport and specify the host and port to connect to
 const UDPSerialPort = new VirtualSerialPort({
     host: '192.168.4.1',
@@ -11,6 +72,8 @@ const UDPSerialPort = new VirtualSerialPort({
 console.log('init');
 
 const nodeMCUBoard = new FirmataBoard(UDPSerialPort);
+
+
 
 nodeMCUBoard.once('ready', function () {
     console.log('NodeMCU ready!');
@@ -30,14 +93,22 @@ nodeMCUBoard.once('ready', function () {
     arduinoUno.on('ready', function () {
         console.log('UNO ready!');
 
+        let currentColor = '000000';
         const rgb = new RGBLed([5, 6, 3]);
-        const randomColor = () => Math.random().toString(16).substr(-6);
 
-        this.loop(100, function () {
-            const countedColor = randomColor();
-            console.log('countedColor', countedColor);
-            rgb.color(countedColor);
+        ioServer.on('connection', function (socket) {
+            socket.on('message', function (data) {
+                const {color} = data
+                console.log(color);
+                rgb.color(color);
+            });
         });
+
+        // this.loop(100, function () {
+        //     const countedColor = '112233';
+            console.log('countedColor', currentColor);
+            rgb.color(currentColor);
+        // });
     });
 
 });
